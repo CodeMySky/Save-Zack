3069375aa67f85ec50fb5356bc8473c133a3dd6a
root <root@AY130111075717b6a5055.(none)>
2013-09-22 21:15:12 +0800

    add images

--- app.js
+++ app.js
@@ -65,6 +65,6 @@ app.configure('production', function() {
 module.exports = app;
 
 if (!module.parent) {
-  app.listen(80);
+	app.listen(app.get('port'));
   console.log("Express server listening on port %d in %s mode", app.get('port'), app.settings.env)
 }
